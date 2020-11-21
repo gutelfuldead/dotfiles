948ad961337a06161f76bde134dfab31e20bb1b0
Jason Gutel <jason.gutel@gmail.com>
2020-11-21 01:20:18 -0800

    moved non-pacman apps install to their own functino

--- install.sh
+++ install.sh
@@ -48,7 +48,8 @@ centosApps="perl-Tk-devel.x86_64 \
 
 # apps to install if using ubuntu
 ubuntuApps="docutils-common \
-    g++
+    g++ \
+    yakuake
     "
 
 # apps to install if using arch
@@ -141,6 +142,93 @@ archAurInstall() {
     cd $here
 }
 
+non_pacman_apps () {
+    use_git=0
+    ################################################################################
+    # install non-pacman apps option
+    ################################################################################
+    read -r -p "use git to source latest builds? If not tarballs will be used [y/n] : " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            use_git=1
+            ;;
+        [nN][oO]|[nN])
+            use_git=0
+            ;;
+        *)
+            echo "Must choose y/n..."
+            exit 1
+            ;;
+    esac
+
+    ################################################################################
+    # fzf
+    ################################################################################
+    tmp=$(which fzf > /dev/null 2>&1)
+    if [ $? -ne 0 ]; then
+        if [ $arch -eq 1 ]; then
+            sudo pacman -S fzf
+        else
+            echon "installing fzf ..."
+            if [ $use_git -eq 1 ]; then
+                git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf | tee -a $logfile
+                ~/.fzf/install | tee -a $logfile
+            else
+                mkdir -pv ~/.fzf
+                cd ~/.fzf
+                curl -LO https://github.com/junegunn/fzf/archive/0.21.1.zip | tee -a $logfile
+                unzip 0.21.1.zip | tee -a $logfile
+                ./fzf-0.21.1/install | tee -a $logfile
+            fi
+            cd $here
+        fi
+    fi
+
+    ################################################################################
+    # rcm
+    ################################################################################
+    tmp=$(which rcup > /dev/null 2>&1)
+    if [ $? -ne 0 ]; then
+        echon "installing rcm ..."
+        if [ $debian -eq 1 ]; then
+            wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add - | tee -a $logfile
+            echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list | tee -a $logfile
+            sudo apt-get update | tee -a $logfile
+            sudo apt-get install rcm | tee -a $logfile
+        else
+            mkdir -p ~/.rcm
+            cd ~/.rcm
+            curl -LO https://thoughtbot.github.io/rcm/dist/rcm-1.3.3.tar.gz | tee -a $logfile
+            tar -xvf rcm-1.3.3.tar.gz | tee -a $logfile
+            cd rcm-1.3.3
+            ./configure | tee -a $logfile
+            make | tee -a $logfile
+            sudo make install | tee -a $logfile
+            cd $here
+        fi
+    fi
+
+    ################################################################################
+    # ranger
+    ################################################################################
+     tmp=$(which ranger > /dev/null 2>&1)
+     if [ $? -ne 0 ]; then
+        echon "installing ranger ..."
+        if [ $use_git -eq 1 ]; then
+            git clone git@github.com:ranger/ranger.git ~/.ranger | tee -a $logfile
+            sudo make -C ~/.ranger install | tee -a $logfile
+        else
+            mkdir -p ~/.ranger
+            cd ~/.ranger
+            curl -LO https://github.com/ranger/ranger/archive/v1.9.3.zip | tee -a $logfile
+            unzip v1.9.3.zip | tee -a $logfile
+            cd ranger-1.9.3
+        fi
+        sudo make install | tee -a $logfile
+        cd $here
+     fi
+}
+
 ################################################################################
 # start main
 ################################################################################
@@ -186,25 +274,11 @@ if [ $distro == "" ]; then
     exit 1
 fi
 
-read -r -p "use git to source latest builds? If not tarballs will be used [y/n] : " response
-case "$response" in
-    [yY][eE][sS]|[yY])
-        use_git=1
-        ;;
-    [nN][oO]|[nN])
-        use_git=0
-        ;;
-    *)
-        echo "Must choose y/n..."
-        exit 1
-        ;;
-esac
-
+################################################################################
+# install pacman apps
+################################################################################
 echon "Installing on $distro ..."
 
-################################################################################
-# install common apps
-################################################################################
 echon "installing and updating apps with $tool ..."
 if [ $arch -eq 1 ]; then
     sudo pacman -S $applist | tee -a $logfile
@@ -221,71 +295,9 @@ else
 fi
 
 ################################################################################
-# fzf
+# Install non package manager apps
 ################################################################################
-tmp=$(which fzf > /dev/null 2>&1)
-if [ $? -ne 0 ]; then
-    if [ $arch -eq 1 ]; then
-        sudo pacman -S fzf
-    else
-        echon "installing fzf ..."
-        if [ $use_git -eq 1 ]; then
-            git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf | tee -a $logfile
-            ~/.fzf/install | tee -a $logfile
-        else
-            mkdir -pv ~/.fzf
-            cd ~/.fzf
-            curl -LO https://github.com/junegunn/fzf/archive/0.21.1.zip | tee -a $logfile
-            unzip 0.21.1.zip | tee -a $logfile
-            ./fzf-0.21.1/install | tee -a $logfile
-        fi
-        cd $here
-    fi
-fi
-
-################################################################################
-# rcm
-################################################################################
-tmp=$(which rcup > /dev/null 2>&1)
-if [ $? -ne 0 ]; then
-    echon "installing rcm ..."
-    if [ $debian -eq 1 ]; then
-        wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add - | tee -a $logfile
-        echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list | tee -a $logfile
-        sudo apt-get update | tee -a $logfile
-        sudo apt-get install rcm | tee -a $logfile
-    else
-        mkdir -p ~/.rcm
-        cd ~/.rcm
-        curl -LO https://thoughtbot.github.io/rcm/dist/rcm-1.3.3.tar.gz | tee -a $logfile
-        tar -xvf rcm-1.3.3.tar.gz | tee -a $logfile
-        cd rcm-1.3.3
-        ./configure | tee -a $logfile
-        make | tee -a $logfile
-        sudo make install | tee -a $logfile
-        cd $here
-    fi
-fi
-
-################################################################################
-# ranger
-################################################################################
- tmp=$(which ranger > /dev/null 2>&1)
- if [ $? -ne 0 ]; then
-    echon "installing ranger ..."
-    if [ $use_git -eq 1 ]; then
-        git clone git@github.com:ranger/ranger.git ~/.ranger | tee -a $logfile
-        sudo make -C ~/.ranger install | tee -a $logfile
-    else
-        mkdir -p ~/.ranger
-        cd ~/.ranger
-        curl -LO https://github.com/ranger/ranger/archive/v1.9.3.zip | tee -a $logfile
-        unzip v1.9.3.zip | tee -a $logfile
-        cd ranger-1.9.3
-    fi
-    sudo make install | tee -a $logfile
-    cd $here
- fi
+non_pacman_apps
 
 ################################################################################
 # install all arch AUR apps
