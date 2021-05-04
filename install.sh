3a1759b097b6a282a51e40b737946651b9b34723
Jason Gutel <jason.gutel@gmail.com>
2021-05-04 11:14:50 -0700

    removed sudo from pip command

--- install.sh
+++ install.sh
@@ -165,7 +165,7 @@ installAppList() {
                             sudo python3 -m pip install --upgrade pip | tee -a $logfile
                             pipInit=1
                         fi
-                        sudo pip install -U $app | tee -a $logfile
+                        pip install -U $app | tee -a $logfile
                     fi
                     ;;
                 GP ) # append group list, dont add now wait for everything to be installed, just aggregate
